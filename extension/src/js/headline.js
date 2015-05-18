a778ffb33d825c609b23164682e792a7e1350dee
ShivanKaul <shivankaul.1993@gmail.com>
2015-05-18 17:47:24 -0400

    Add gitignore and refactor headline.js code

--- extension/src/js/headline.js
+++ extension/src/js/headline.js
@@ -10,23 +10,18 @@ function call_api() {
 	        	}
 	        },
 			success: function (queryResult) {
-				// get array of all software projects
+				// get array of all headlines
 				var results = queryResult.results;
 				var numResults = queryResult.num_results;
 				if (numResults > 0) {
-					var title = "Letters to the Editor";
-					while (title == "Letters to the Editor") {
-						var bound = Math.min(19, numResults);
-						var randomNum = Math.floor((Math.random()*bound));
-						console.log(randomNum);
-						title = results[randomNum].title;
-					}
+					var result = getTitle(numResults, results)
+					var title = result[0]
+					var randomNum = result[1]
 					var link = results[randomNum].url;
 					var abstract = results[randomNum].abstract;
 					document.getElementById("insert").setAttribute('href', link);
 					document.getElementById("insert").innerHTML = title;
 					document.getElementById("abstract").innerHTML = abstract;
-					// document.write(title);
 				}
 				else {
 					document.write("No news found!");
@@ -34,6 +29,18 @@ function call_api() {
 			}
 		});
 	});
+}
+
+function getTitle(numResults, results) {
+	var bound = Math.min(19, numResults);
+	var randomNum = Math.floor((Math.random()*bound));
+	var title = results[randomNum].title;
+	while (title == "Letters to the Editor" || title == "Reactions") {
+		var newResult = getTitle(numResults, results)
+		title = newResult[0];
+		randomNum = newResult[1]
+	}
+	return [title, randomNum]
 }
 
 call_api();
